42be06fe71623186a09033007a765e460991fc73
lisbeth mbuvi <lisbeth.mbuvi@mawingunetworks.com>
2020-05-18 07:11:23 +0300

    Updates

---
+++ precache-manifest.afa3c2af7cff91000ce3d9d897871ada.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22180087a16c9eef9d878d446c81c1c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b84a74618d5f8ef944ab",
+    "url": "/Portfolio/static/css/2.20155695.chunk.css"
+  },
+  {
+    "revision": "b80693b1fa512c04c49d",
+    "url": "/Portfolio/static/css/main.6f752875.chunk.css"
+  },
+  {
+    "revision": "b84a74618d5f8ef944ab",
+    "url": "/Portfolio/static/js/2.c77e15e6.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.c77e15e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80693b1fa512c04c49d",
+    "url": "/Portfolio/static/js/main.343cc6e0.chunk.js"
+  },
+  {
+    "revision": "6993f65de5f774e6d08a",
+    "url": "/Portfolio/static/js/runtime-main.34481503.js"
+  },
+  {
+    "revision": "412e1ef23903729529ad558221be0b59",
+    "url": "/Portfolio/static/media/Developer.412e1ef2.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a08d04de999e9c4ee5262d83da874513",
+    "url": "/Portfolio/static/media/mawingu.a08d04de.png"
+  },
+  {
+    "revision": "25e63889dbfb6aef7aea8352ef60dbae",
+    "url": "/Portfolio/static/media/react-logo.25e63889.png"
+  }
+]);
